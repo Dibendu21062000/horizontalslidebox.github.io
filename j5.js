6265953f582e373149b7effeed20afa2301e1b98
Dibendu21062000 <72265630+Dibendu21062000@users.noreply.github.com>
2022-07-05 03:15:58 +0530

    horizontalslidebox

---
+++ j5.js
@@ -0,0 +1,9 @@
+function scrollleft(){
+    var left = document.querySelector(".slidebox");
+    left.scrollBy(250,0);
+}
+
+function scrollright(){
+    var right = document.querySelector(".slidebox");
+    right.scrollBy(-250,0);
+}
